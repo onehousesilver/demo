c68659953eb682f76f648834194c263c478a98f7
hanchaeeun <onehousesilver@gmail.com>
2022-07-01 18:01:18 +0900

    html

--- demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-
+    @Autowired
     private final MemberRepository memberRepository;
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
